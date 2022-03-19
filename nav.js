837004718cb947e5706fbc0485b4e2cddf9e70a6
cephasmutua19 <cephasmutua1984@gmail.com>
2022-03-19 09:04:57 +0300

    Initial commit

---
+++ nav.js
@@ -0,0 +1,14 @@
+const navLink = document.querySelector("#nav-link");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener('click', () => {
+    const visibility = navLink.getAttribute('data-visible');
+
+    if(visibility === "false"){
+        navLink.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else{
+        navLink.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    };
+});
